eb2172a82026eac40e27889a364f84b73d605955
Nikita Bukhta <nikita.bukhta.dev@gmail.com>
2023-07-30 21:51:45 +0300

    [Messenger-Client][Client][controllers]
    Injected logging to controllers

--- Messenger-Client/Client/Controllers/ClientController/include/ClientController.hpp
+++ Messenger-Client/Client/Controllers/ClientController/include/ClientController.hpp
@@ -8,7 +8,6 @@
 
 #include <memory>
 #include <string>
-#include <iostream>
 
 #include <inttypes.h>
 
--- Messenger-Client/Client/Controllers/ClientController/src/ClientController.cpp
+++ Messenger-Client/Client/Controllers/ClientController/src/ClientController.cpp
@@ -1,8 +1,6 @@
 #include "ClientController.hpp"
 
-#include <boost/log/trivial.hpp>
-#include <boost/core/demangle.hpp>
-#include <boost/current_function.hpp>
+#include "Logger.hpp"
 
 namespace bukhtagram {
 namespace mc {
@@ -12,32 +10,41 @@ namespace controllers {
 ClientController::ClientController(std::weak_ptr<models::IClientModel> client_model)
     : m_client_model(client_model.lock())
 {
-
+    DECLARE_TAG_SCOPE;
+    LOG_INFO << "called";
 }
 
 ClientController::~ClientController(void) {
+    DECLARE_TAG_SCOPE;
+    LOG_INFO << "called";
+
     auto socket = m_client_model->socket().lock();
     socket->close();
 }
 
 void ClientController::connect_to(const std::string &address, uint16_t port) {
+    DECLARE_TAG_SCOPE;
+    LOG_INFO << "called";
     boost::system::error_code error_code;
     auto socket = m_client_model->socket().lock();
     boost::asio::ip::tcp::endpoint server_endpoint(
         boost::asio::ip::address::from_string(address), port
     );
     socket->connect(server_endpoint, error_code);
-    handle_error(error_code);
+    if (!handle_error(error_code)) {
+        LOG_INFO << "connected!";
+    }
 }
 
 bool ClientController::handle_error(boost::system::error_code &error) {
-    std::cout << "Called handle_error with error #" << error.value() << std::endl;
+    DECLARE_TAG_SCOPE;
+
     if (error.value() == boost::system::errc::success) {
+        LOG_TRACE << "no errors handled";
         return false;
     }
 
-    BOOST_LOG_TRIVIAL(info) << error.what();
-
+    LOG_ERROR << error.what();
     return true;
 }
 
